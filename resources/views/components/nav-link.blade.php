2ad1456d3ac40845a2d9de4d607ba493ec95776a
obinnaakaolisa <akaolisaobinna@gmail.com>
2024-10-17 19:24:59 +0100

    ADD: nav-link component and props

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,3 +1,8 @@
-<a {{$attributes}}>
+@props(['url' => '/', 'active' => true, 'icon' => null])
+
+<a href="{{$url}}" class="text-white hover:underline py-2 {{$active ? 'text-yellow-500 font-bold' : ''}}">
+    @if ($icon)
+        <i class="fa-solid fa-{{$icon}} mr-1"></i>
+    @endif
     {{$slot}}
 </a>
